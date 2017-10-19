0e05fa1e09ce469c521de04e730064f0b7802841
AdrieanKhisbe <adriean.khisbe@live.fr>
2017-10-19 08:08:10 +0200

    feature: add a last-quiet-accept line bound to ^x^k

--- quiet-accept-line.zsh
+++ quiet-accept-line.zsh
@@ -12,6 +12,7 @@ function quiet-accept-line () {
     else PROMPT="$(repeat $(($nline -1)) echo)"
     fi
     # Backup and reset current buffer
+    ZLE_LAST_QUIET_ACCEPT_LINE=$BUFFER
     local _BUFFER=$BUFFER
     BUFFER=""
     # Erase current prompt
@@ -27,8 +28,15 @@ zle -N quiet-accept-line
 bindkey '^X^M' quiet-accept-line
 
 function silent-accept-line () {
+    ZLE_LAST_QUIET_ACCEPT_LINE=$BUFFER
     eval $BUFFER > /dev/null
     BUFFER=""
 }
 zle -N silent-accept-line
 bindkey '^X^J' silent-accept-line
+
+function last-quiet-accept-line () {
+    BUFFER=$ZLE_LAST_QUIET_ACCEPT_LINE
+}
+zle -N last-quiet-accept-line
+bindkey '^X^K' last-quiet-accept-line
