4c7524cfd69591327273841dfc454b3f9cf76d53
AdrieanKhisbe <adriean.khisbe@live.fr>
2017-10-20 01:06:58 +0200

    test: first test to cover silent/quiet-line accept

---
+++ shpec/quiet_accept_line_shpec.zsh
@@ -0,0 +1,63 @@
+# -*- coding: utf-8 -*-
+# Spec for the zle widgets
+
+setopt aliases
+source $(dirname $0:A)/../quiet-accept-line.zsh
+
+OUTPUT=$(mktemp)
+ZLE_CALL=()
+stub_command zle 'ZLE_CALL+=( "$1 \"$PROMPT\"" )'
+reset_stub_zle() { ZLE_CALL=(); }
+
+with_stub_eval() {
+    stub_command eval 'EVAL="$@"; $@'
+    $1
+    unstub_command eval
+}
+
+describe "Quiet Accept line"
+    it "reset prompy correctly"
+        PROMPT=">"
+        with_stub_eval quiet-accept-line
+        assert equal "${ZLE_CALL[1]}" 'reset-prompt ""'
+        assert equal "${ZLE_CALL[2]}" 'reset-prompt ">"'
+        reset_stub_zle
+    end
+
+    it "run BUFFER as a command"
+        BUFFER="echo yo"
+        with_stub_eval quiet-accept-line > $OUTPUT
+        output=$(cat $OUTPUT)
+        assert equal "$EVAL" "echo yo"
+        assert equal "$output" "yo"
+        reset_stub_zle
+    end
+
+    it "empty buffer after run"
+        BUFFER="echo yo"
+        with_stub_eval quiet-accept-line > $OUTPUT
+        output=$(cat $OUTPUT)
+        assert equal "$BUFFER" ""
+        assert equal "$output" "yo"
+        reset_stub_zle
+    end
+end
+
+describe "Silent Accept line"
+
+    it "run BUFFER as a command, quietly"
+        BUFFER="echo yo"
+        with_stub_eval silent-accept-line > $OUTPUT
+        output=$(cat $OUTPUT)
+        assert equal "$EVAL" "echo yo"
+        assert equal "$output" ""
+        reset_stub_zle
+    end
+
+    it "empty buffer after run"
+        BUFFER="echo yo"
+        with_stub_eval silent-accept-line
+        assert equal "$BUFFER" ""
+        reset_stub_zle
+    end
+end
